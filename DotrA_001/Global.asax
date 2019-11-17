21da19df6f51f6ecd6997463a435c44114e5c282
MengHanChen <zore790621@gmail.com>
2019-11-18 00:06:16 +0800

    加入專案檔案。

---
+++ DotrA_001/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotrA_001.MvcApplication" Language="C#" %>
---
+++ DotrA_001/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotrA_001.Startup))]
+namespace DotrA_001
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
