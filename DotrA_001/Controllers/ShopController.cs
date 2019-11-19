8a5772411903649ba8b684808c53142d0ac63027
ztex11020676522@gmail.com <ztex11020676522@gmail.com>
2019-11-19 17:54:45 +0800

    Shopview分類為Partial方式

---
+++ DotrA_001/Controllers/ShopController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotrA_001.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Single_Product()
+        {
+            return View();
+        }
+        public ActionResult Categories()
+        {
+            return View();
+        } 
+        public ActionResult PageSelection()
+        {
+            return View();
+        }
+        public ActionResult PriceRange()
+        {
+            return View();
+        }
+    }
+}
