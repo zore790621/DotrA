27873478549bdf78634f2ef9d0717c8f891c2af1
One <chjh8250@gmail.com>
2019-11-20 19:11:26 +0800

    test member

---
+++ DotrA_001/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotrA_001.Models
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+        public string MemberAccount { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+    }
+}
