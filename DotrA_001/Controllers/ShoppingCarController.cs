d35333feae0f8605447b92b8f80e802593382835
ztex11020676522@gmail.com <ztex11020676522@gmail.com>
2019-11-20 18:46:10 +0800

    建立Shoppcar_View及Shop的圖片購物車能連接至Shoppingcar頁面

---
+++ DotrA_001/Controllers/ShoppingCarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotrA_001.Controllers
+{
+    public class ShoppingCarController : Controller
+    {
+        // GET: ShoppingCar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
