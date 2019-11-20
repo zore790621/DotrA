ad3952c48ccc22c1f02591f54ad8032189f901ed
liuszuyu <mimiliuszuyu@hotmail.com>
2019-11-20 18:59:30 +0800

    新增Members Model

---
+++ DotrA_001/Models/Member.cs
@@ -0,0 +1,38 @@
+namespace DotrA_001.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Member
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int MemberID { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string MemberAccount { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Phone { get; set; }
+    }
+}
