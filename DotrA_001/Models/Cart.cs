9e0da9053cea12ff65e2b0688722936cb6555a45
even85125566 <even85125566@gmail.com>
2019-11-21 13:46:24 +0800

    2019/11/21下午兩點

---
+++ DotrA_001/Models/Cart.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotrA_001.Models
+{
+    //可序列化
+    [Serializable]
+    public class Cart:IEnumerable<CartProduct>
+    {
+        public Cart()
+        {
+            this.CartProducts = new List<CartProduct>();
+        }
+        public List<CartProduct> CartProducts;
+
+
+        //計算商品總數
+        public int count {
+            get {
+                return this.CartProducts.Count;
+            }
+        
+        }
+
+
+        //此次購買總價
+        public decimal TotalAmount
+        {
+            get {
+                 decimal totalamount=0m;
+                foreach(var caritem in this.CartProducts)
+                    {
+                    totalamount = totalamount + caritem.Amount;
+                }
+                return totalamount;
+            }
+        }
+
+        public IEnumerator<CartProduct> GetEnumerator()
+        {
+            return ((IEnumerable<CartProduct>)CartProducts).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<CartProduct>)CartProducts).GetEnumerator();
+        }
+    }
+}
---
+++ DotrA_001/Models/Justfortest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotrA_001.Models
+{
+    public class Justfortest
+    {
+        
+    }
+}
