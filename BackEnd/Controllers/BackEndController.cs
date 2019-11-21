ac3e61278450aed325b7884da1774eac10a774de
liuszuyu <mimiliuszuyu@hotmail.com>
2019-11-21 13:30:06 +0800

    完成初步admin登入畫面

---
+++ BackEnd/Controllers/BackEndController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotrA_001.Controllers
+{
+    public class BackEndController : Controller
+    {
+        // GET: BackEnd
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ BackEndSystem/Models/LogInRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSystem.Models
+{
+    public class LogInRequest
+    {
+        public string AdminAccount { get; set; }
+        public string AdminPW { get; set; }
+    }
+}
