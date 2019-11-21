7ad33cfe00750181f4227dc33229b413ce72a4ef
One <chjh8250@gmail.com>
2019-11-21 13:30:49 +0800

    加入Identity 套件 註冊和登入頁面

--- DotrA_001/Models/Member.cs
+++ DotrA_001/Models/Member.cs
@@ -8,30 +8,33 @@ namespace DotrA_001.Models
 
     public partial class Member
     {
+        [Display(Name = "會員編號")]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int MemberID { get; set; }
-
+        
+        [Display(Name = "帳號")]
         [Required]
         [StringLength(20)]
         public string MemberAccount { get; set; }
-
+        [Display(Name = "密碼")]
         [Required]
         [StringLength(20)]
         public string Password { get; set; }
-
+        [Display(Name = "姓名")]
         [Required]
         [StringLength(50)]
         public string Name { get; set; }
-
+        [Display(Name = "信箱")]
         [Required]
         [StringLength(50)]
         public string Email { get; set; }
-
+        [Display(Name = "地址")]
         [Required]
         [StringLength(50)]
         public string Address { get; set; }
-
+        [Display(Name = "行動電話")]
         [Required]
+        [RegularExpression(@"^\d{4}\-?\d{3}\-?\d{3}$", ErrorMessage = "需為09xx-xxx-xxx格式")]
         [StringLength(20)]
         public string Phone { get; set; }
     }
