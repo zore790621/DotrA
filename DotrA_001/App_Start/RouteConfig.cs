5ff39c660f6481260b3078bf32751896ee5702c1
ztex11020676522@gmail.com <ztex11020676522@gmail.com>
2019-11-21 13:35:22 +0800

    text

--- DotrA_001/App_Start/RouteConfig.cs
+++ DotrA_001/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DotrA_001
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Shop", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
