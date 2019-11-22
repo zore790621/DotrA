b42098ed314f12533c0747bec641694d4981b8c2
zore790621 <zore790621@gmail.com>
2019-11-22 21:43:55 +0800

    Revert "還原master"
    This reverts commit 9ef6028d0432d7243a873bb54f9d95232fd4bf69.

---
+++ DotrA_001/Controllers/TestCartController.cs
@@ -0,0 +1,54 @@
+﻿using DotrA_001.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotrA_001.Controllers
+{
+    public class TestCartController : Controller
+    {
+        // GET: TestCart
+        public ActionResult GetCart()
+        {
+            var cart = HandleCart.GetCurrentCart();
+            //若現在購物車沒有商品的話，新增一個假商品(為了練習)
+            if (cart.CartProducts.Count == 0)
+            {
+             
+                cart.CartProducts.Add(
+                    new Models.CartProduct()
+                    {
+                        ProductId = 1,
+                        ProductName = "測試商品",
+                        ProductPrice = 50m,
+                        ProductQuantity = 1
+                    }
+                    );
+                cart.CartProducts.Add(
+                   new Models.CartProduct()
+                   {
+                       ProductId = 2,
+                       ProductName = "測試商品2",
+                       ProductPrice = 3m,
+                       ProductQuantity = 1
+                   }
+                   );
+            }
+            //每當你重載頁面，商品數量多+一個(session儲存的緣故)
+            else
+            {
+                foreach (var i in cart.CartProducts)
+                {
+                    i.ProductQuantity++;
+                }
+            }
+            return Content(string.Format($"目前金額為{cart.TotalAmount}元"));
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
