a4c6325f349f95692566fe47c56f079c164e0f11
liuszuyu <mimiliuszuyu@hotmail.com>
2019-11-28 02:12:26 +0800

    連接到DotrA Context、初步完成會員列表

---
+++ BackEndSystem/Models/DotrAContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSystem.Models
+{
+    public class DotrAContext : DbContext
+    {
+        public DotrAContext():base("DotrAContext")
+        {
+
+        }
+
+        public DbSet<Member> Members { get; set; }
+    }
+
+   
+}
