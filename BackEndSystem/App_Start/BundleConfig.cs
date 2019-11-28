a25ab8f74e7864e7c54017c672e65d938533fa1d
liuszuyu <mimiliuszuyu@hotmail.com>
2019-11-28 15:09:54 +0800

    Fix 庫存刪除功能

--- BackEndSystem/App_Start/BundleConfig.cs
+++ BackEndSystem/App_Start/BundleConfig.cs
@@ -41,8 +41,8 @@ namespace BackEndSystem
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/normalize.css",
