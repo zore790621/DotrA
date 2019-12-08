05c1dca557c57f435a826398af24384714aa90e1
One <chjh8250@gmail.com>
2019-12-08 22:25:04 +0800

    modal 測試 第二版

--- DotrA_001/Global.asax.cs
+++ DotrA_001/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace DotrA_001
 {
@@ -19,5 +21,22 @@ namespace DotrA_001
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        //void Application_AuthenticateRequest(object sender, EventArgs e)
+        //{
+        //    if (Request.IsAuthenticated)
+        //    {
+        //        // 先取得該使用者的 FormsIdentity
+        //        FormsIdentity id = (FormsIdentity)User.Identity;
+        //        // 再取出使用者的 FormsAuthenticationTicket
+        //        FormsAuthenticationTicket ticket = id.Ticket;
+        //        // 將儲存在 FormsAuthenticationTicket 中的角色定義取出，並轉成字串陣列
+        //        string[] roles = ticket.UserData.Split(new char[] { ',' });
+        //        // 指派角色到目前這個 HttpContext 的 User 物件去
+        //        //剛剛在創立表單的時候，你的UserData 放使用者名稱就是取名稱，我放的是群組代號，所以取出來就是群組代號
+        //        //然後會把這個資料放到Context.User內
+        //        Context.User = new GenericPrincipal(Context.User.Identity, roles);
+        //    }
+
+        //}
     }
 }
