d6b2752dea1dff3301b3b54437fa5cf36437bc88
One <chjh8250@gmail.com>
2019-12-09 14:22:06 +0800

    修改modal按鈕

--- DotrA_001/App_Start/RouteConfig.cs
+++ DotrA_001/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DotrA_001
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Members", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
