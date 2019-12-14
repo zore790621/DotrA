12c9d169946532c914d66fe4d6ddc3406b8a7dc2
temu chen <temu84523@gmail.com>
2019-12-14 14:46:39 +0800

    儀錶板1214

--- BackEndSystem/Controllers/HomeController.cs
+++ BackEndSystem/Controllers/HomeController.cs
@@ -10,9 +10,25 @@ namespace BackEndSystem.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        private DotrAContext db = new DotrAContext();
         public ActionResult Index()
         {
-            return View();
+            //方法1 透過modle去傳遞
+            Home result = new Home()
+            {
+                Selltotal = db.Products.Count(),
+                Ordertotal = db.Categories.Count(),
+                Prototal = db.Orders.Count()
+            };
+
+            //方法2 透過ViewBag去傳遞
+            ViewBag.ProCount = db.Products.Count();
+            ViewBag.CatCount = db.Categories.Count();
+            ViewBag.OrdCount = db.Orders.Count();
+
+
+
+            return View(result);
         }
 
         //public ActionResult Member()
---
+++ BackEndSystem/Models/Home.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSystem.Models
+{
+    public class Home
+    {
+        public int Prototal { get; set; }
+        public int Cattotal { get; set; }
+        public int Ordertotal { get; set; }
+        public int Selltotal { get; set; }
+
+        public int GetTotal()
+        {
+            return Prototal + Cattotal + Ordertotal + Selltotal;
+        }
+    }
+}
