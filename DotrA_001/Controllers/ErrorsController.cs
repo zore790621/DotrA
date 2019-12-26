e1a9f0a327effb9ed152e217fb3c2c373d0d38a7
Harry Chen <zore790621@gmail.com>
2019-12-26 15:29:55 +0800

    增加Error處裡

---
+++ DotrA_001/Controllers/ErrorsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotrA_001.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        public ActionResult Index()
+        {
+            this.Response.StatusCode = 500;
+
+            return this.View();
+        }
+        public ActionResult Forbidden()
+        {
+            this.Response.StatusCode = 403;
+
+            return this.View();
+        }
+
+        public ActionResult NotFound()
+        {
+            this.Response.StatusCode = 404;
+
+            return this.View();
+        }
+
+        public ActionResult Other(int? code)
+        {
+            this.Response.StatusCode = code ?? 500;
+
+            return this.View();
+        }
+    }
+}
